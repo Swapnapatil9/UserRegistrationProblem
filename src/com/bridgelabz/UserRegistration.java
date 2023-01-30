cd9d85804856a5d95f96fa6262809e51073ffd46
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-01-30 19:37:42 +0530

    Added:User need to enter a valid first name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,12 @@
 package com.bridgelabz;
 
 public class UserRegistration {
+    // Validate user first name
+    public static boolean isValidFirstName(String fName){
+        return fName.matches("[A-Z]{1}[a-z]{2,}");
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem using Regular expression");
+        System.out.println(isValidFirstName("Swapna"));
     }
 }
